387a648646cfa874b9bf8094279267a3aae27d5b
Elysian01 <abhig0209@gmail.com>
2023-09-09 19:45:23 +0530

    trial

--- Hands-on-List-1/11/11.c
+++ Hands-on-List-1/11/11.c
@@ -19,7 +19,11 @@ int main(int argc, char **argv)
         perror("Problem in opening the file");
         return 0;
     }
-
+    // dup, will create the duplicate of file descriptor with availabe FD numbers
+    // we have 3 system calls to do this operation
+    // fcntl, dup, dup2
+    // fcntl and dup is same it will create a new FD say 6, which will point to the contents of the input FD
+    // in dup2, we can specify the FD value explicitly, like below we specically want 7 as FD value
     fd1 = fcntl(fd, F_DUPFD);
     if (fd1 == -1)
     {
@@ -36,15 +40,21 @@ int main(int argc, char **argv)
     }
     write(fd1, "to\n", 3);
 
-    fd3 = dup2(fd, 6);
+    fd3 = dup2(fd, 7);
     if (fd3 == -1)
     {
         perror("Problem in duplicating fd using dup2");
         return 0;
     }
     write(fd1, "SS Lab\n", 7);
-    close(fd);
-    close(fd1);
-    close(fd2);
-    close(fd3);
+    // while (1)
+    //     ;
+
+    // ./a.out filename &
+    // ll /proc/<pid>/fd  => to check the linkage
+
+    // close(fd);
+    // close(fd1);
+    // close(fd2);
+    // close(fd3);
 }
---
+++ Hands-on-List-1/12/12.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+int main()
+{
+    int fd = open("temp.txt", O_WRONLY);
+    if (fd == -1)
+    {
+        printf("Error opening first file\n");
+        close(fd);
+        return 0;
+    }
+    int mode = fcntl(fd, F_GETFL); // returns the mode number of the opened file
+    mode = mode & O_ACCMODE;       // now we are mapping number to r/w flags
+    if (mode == O_RDONLY)
+    {
+        printf("Opened in read mode\n");
+    }
+    else if (mode == O_WRONLY)
+    {
+        printf("Opened in write mode\n");
+    }
+    else
+    {
+        printf("Opened in read write mode\n");
+    }
+    close(fd);
+}
